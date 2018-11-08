9714f72b537d08556fa9a4a34034a5ea6ad9e23b
Harris Lapiroff <harris@chromamine.com>
2018-11-07 20:02:57 -0500

    Improve chart's mobile friendliness
    Eventually it would be nice to trigger these recalculations when the window resizes. For now doing it upon initial load is good enough

--- client/src/components/TweetMultiBarChart/measurements.js
+++ client/src/components/TweetMultiBarChart/measurements.js
@@ -3,6 +3,6 @@ export const ROW_HEIGHT = BAR_HEIGHT * 2
 export const ROW_GUTTER = 4
 export const LABEL_WIDTH = 75
 export const GUTTER = 10
-export const BAR_MAX_WIDTH = 300
+export const BAR_MAX_WIDTH = window.innerWidth > 450 ? 300 : 150
 export const COUNT_LABEL_WIDTH = 75
 export const CHART_WIDTH = LABEL_WIDTH * 2 + GUTTER * 2 + BAR_MAX_WIDTH
