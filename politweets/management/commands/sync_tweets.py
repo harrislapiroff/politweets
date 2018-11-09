7f3d32cb439fa4ef38c38144c38bc9e3c90d14ce
Harris Lapiroff <harris@chromamine.com>
2018-11-09 14:26:06 -0500

    Record full text of tweets
    Also add an --overwrite flag to sync_tweets. This will allow us to
    delete and update older tweets in the database so they include the
    new data instead. It will miss a few types of tweets though:
    
    - Any tweets sufficiently old or from sufficiently prolific
      tweeters that they are past the 200 tweet timeline limit. We may
      be able to update those tweets specifically later on by querying
      for truncated tweets
    - Any tweets that have since been deleted and no longer exist on
      twitter.com. Haven't figured out a good way to handle those yet
      for this purpose or any other.

--- politweets/management/commands/sync_tweets.py
+++ politweets/management/commands/sync_tweets.py
@@ -6,8 +6,16 @@ from politweets.utils.sync_tweets import sync_all_tweets, SUCCESS
 class Command(BaseCommand):
     help = 'Sync tweets from the Twitter API'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--overwrite', '-O',
+            action='store_true',
+            dest='overwrite',
+            default=False
+        )
+
     def handle(self, *args, **options):
-        for status, member, result in sync_all_tweets():
+        for status, member, result in sync_all_tweets(overwrite=True):
             if status == SUCCESS:
                 self.stdout.write('Synced {} tweets from {} ({})'.format(
                     len(result),
