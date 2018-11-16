28803f3656f1f2b1f20b1a2c9e959410d59b88ce
Harris Lapiroff <harris@chromamine.com>
2018-11-16 16:58:00 -0500

    Make tweet list animate

--- client/src/components/TweetList.js
+++ client/src/components/TweetList.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import posed, { PoseGroup } from 'react-pose'
 
 import Tweet from '~/components/Tweet'
 
+const TweetWrapper = posed.div({
+	enter: { y: 0, opacity: 1, delay: ({ i }) => i * 150 },
+	exit: { y: 50, opacity: 0 },
+})
+
 export default function TweetList({ tweets, classNameFn }) {
-	return tweets.map(t => <Tweet tweet={t} key={t.id} className={classNameFn(t)} />)
+	return (
+		<PoseGroup>
+			{tweets.map((t, i) => (
+				<TweetWrapper key={t.id} i={i}>
+					<Tweet tweet={t} className={classNameFn(t)} />
+				</TweetWrapper>
+			))}
+		</PoseGroup>
+	)
 }
